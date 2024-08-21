95398be2a74afd1b9ec8a146f63727f780121cef
John Kyle Salaysay <foursalaysay@gmail.com>
2024-08-22 00:39:23 +0800

    stuck on creating list

--- src/components/reusables/GooglePlaces.tsx
+++ src/components/reusables/GooglePlaces.tsx
@@ -1,90 +1,42 @@
-"use client";
-import {
-  Command,
-  CommandGroup,
-  CommandItem,
-  CommandList,
-} from "@/components/ui/command";
-import { Input } from "@/components/ui/input";
-import { env } from "process";
-import React, { type KeyboardEvent, useState } from "react";
-import usePlacesService from "react-google-autocomplete/lib/usePlacesAutocompleteService";
+import React from 'react'
+import { GoogleMap, useJsApiLoader, StandaloneSearchBox } from '@react-google-maps/api';
+import { Input } from '../ui/input';
+import { useRef } from 'react';
 
+const GooglePlaces = () => {
+  const inputRef = useRef()
+  const ApiKey = process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY;
 
-type tGooglePredictionManual = {
-  place_id: string;
-  description: string;
-};
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: ApiKey!,
+    libraries : ["places"]
+  })
 
-type GoogleAutoAddressInputProps = {
-  value: string | undefined;
-  onChange: (value: string) => void;
-  disabled?: boolean;
-};
-
-export const GooglePlaces = ({
-  disabled = false,
-  value = undefined,
-  onChange,
-}: GoogleAutoAddressInputProps) => {
-  const [placeListOpen, setPlaceListOpen] = useState(false);
-  const { placePredictions, getPlacePredictions } = usePlacesService({
-    apiKey: env.NEXT_PUBLIC_GOOGLE_API,
-    debounce: 500,
-  });
-
-  // dont know if these are needed
-  const handlePlaceValue = (value: string) => {
-    if (!placeListOpen) setPlaceListOpen(true);
-    getPlacePredictions({ input: value });
-    onChange(value);
-  };
-
-  const handleClose = (e: KeyboardEvent<HTMLInputElement>) => {
-    e.key === "Escape" && setPlaceListOpen(false);
-    e.key === "Tab" && setPlaceListOpen(false);
-  };
-
-  const handleSelect = (placeName: string) => {
-    setPlaceListOpen(false);
-    onChange(placeName);
-  };    
+  const handleOnPlacesChanged = () => {
+    if (inputRef.current) {
+      const address = inputRef.current.getPlaces();
+      console.log(address);
+    }
+  }
 
   return (
-    <Command className="relative overflow-visible">
-      <Input
-        disabled={disabled}
-        placeholder="Search place..."
-        className="w-full"
-        onChange={(e) => handlePlaceValue(e.target.value)}
-        onKeyDown={(e) => handleClose(e)}
-        value={value}
-        type="text"
-        autoComplete="off"
-      />
+    <div>
+      {isLoaded && 
+      <StandaloneSearchBox
+      onLoad={(ref) => inputRef.current = ref } 
+      onPlacesChanged={handleOnPlacesChanged}
 
-      {placeListOpen && (
-        <CommandList
-          className={`absolute left-0 right-0 top-[46px] z-20 rounded-lg bg-background ${
-            placePredictions.length > 0
-              ? "border animate-in fade-in-0 zoom-in-95 "
-              : ""
-          }`}
-        >
-          <CommandGroup
-            className={`${placePredictions.length > 0 ? "p-2" : "p-0"} `}
-          >
-            {placePredictions?.map((place: tGooglePredictionManual) => (
-              <CommandItem
-                key={place.place_id}
-                onSelect={() => handleSelect(place?.description)}
-              >
-                {place.description}
-              </CommandItem>
-            ))}
-          </CommandGroup>
-        </CommandList>
-      )}
-    </Command>
-  );
-};
+      >
+        <Input
+        type='text'
+        placeholder='Enter Address'
+        
+        />
+       </StandaloneSearchBox>
+      }
+    </div>
+  )
+}
+
+export default GooglePlaces
