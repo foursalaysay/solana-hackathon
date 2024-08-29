705bec73806bb74410d19638d60bd5569041ec8f
John Kyle Salaysay <foursalaysay@gmail.com>
2024-08-29 08:09:27 +0800

    adding- button

--- src/components/reusables/ConfirmParticipation.tsx
+++ src/components/reusables/ConfirmParticipation.tsx
@@ -10,6 +10,7 @@ import {
     DialogClose
   } from "@/components/ui/dialog"
 import { Button } from '../ui/button'
+import { Separator } from '../ui/separator'
   
 
   
@@ -17,26 +18,31 @@ export default function ConfirmParticipation() {
   return (
     <>
         <Dialog>
-        <DialogTrigger asChild>
-            <Button variant="outline" className=''>Participate</Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-md">
-            <DialogHeader>
-            <DialogTitle>Share link</DialogTitle>
-            <DialogDescription>
-                Are you sure you want to participate in donating blood?
-            </DialogDescription>
-            </DialogHeader>
-            
-            <DialogFooter className="sm:justify-start">
-            <DialogClose asChild>
-                <Button type="button" variant="secondary">
-                  Close
-                </Button>
-            </DialogClose>
-            </DialogFooter>
-        </DialogContent>
-    </Dialog>
+          <DialogTrigger asChild>
+              <Button variant="default" className=''>Participate</Button>
+          </DialogTrigger>
+          <DialogContent className="w-[320px] lg:w-96 rounded-md">
+              <DialogHeader>
+              <DialogTitle>Share link</DialogTitle>
+              <DialogDescription>
+                  Are you sure you want to participate in donating blood?
+              </DialogDescription>
+              </DialogHeader>
+              <Separator />
+              <DialogFooter className="flex flex-col gap-2">
+                <DialogClose asChild>
+                    <Button type="button" variant="secondary" className='hover:bg-gray-200 w-28'>
+                      Cancel
+                    </Button>
+                </DialogClose>
+                <DialogClose asChild>
+                    <Button type="button" className='w-28 bg-redColor hover:bg-white hover:border-2 hover:border-redColor hover:text-redColor'>
+                      Confirm
+                    </Button>
+                </DialogClose>
+              </DialogFooter>
+          </DialogContent>
+      </Dialog>
     </>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,7 @@ const config = {
     },
     extend: {
       colors: {
+        redColor : "#E10600",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
