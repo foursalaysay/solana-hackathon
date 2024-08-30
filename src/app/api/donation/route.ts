5ad1c00a5586ba8909f1c0f3af908ae1de54fc97
John Kyle Salaysay <foursalaysay@gmail.com>
2024-08-30 15:00:58 +0800

    refined erros for production 1

--- src/app/api/donation/route.ts
+++ src/app/api/donation/route.ts
@@ -1,18 +1,16 @@
-import { NextRequest, NextResponse } from "next/server";
-import { ConnectToDatabase } from "../../../../helpers/server-helper";
+import { NextResponse } from "next/server";
 import prisma from "../../../../prisma";
 
 export const GET = async () => {
+  
   try {
-
-    await ConnectToDatabase();
-    
     const donations = await prisma.donation.findMany();
-    return NextResponse.json({ donations }, { status: 200 });
+
+      return NextResponse.json({ donations }, { status: 200 });
   } catch (error) {
     console.error('Error fetching donations:', error);
+    
+    // Return a server error response
     return NextResponse.json({ message: 'Server Error' }, { status: 500 });
-  } finally {
-    await prisma.$disconnect();
   }
 };
