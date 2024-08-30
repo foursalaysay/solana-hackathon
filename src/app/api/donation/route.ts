8836b91399ecf514334bb14cd96fc442c7df5543
John Kyle Salaysay <foursalaysay@gmail.com>
2024-08-30 23:58:11 +0800

    rebuilding for deplyment

--- src/app/api/donation/route.ts
+++ src/app/api/donation/route.ts
@@ -2,15 +2,11 @@ import { NextResponse } from "next/server";
 import prisma from "../../../../prisma";
 
 export const GET = async () => {
-  
   try {
     const donations = await prisma.donation.findMany();
-
-      return NextResponse.json({ donations }, { status: 200 });
+    return NextResponse.json({ success: true, data: donations }, { status: 200 });
   } catch (error) {
     console.error('Error fetching donations:', error);
-    
-    // Return a server error response
-    return NextResponse.json({ message: 'Server Error' }, { status: 500 });
+    return NextResponse.json({ success: false, message: 'Server Error' }, { status: 500 });
   }
 };
