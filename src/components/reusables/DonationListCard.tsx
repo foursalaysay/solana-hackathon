816b54579bfb7046b16f2c6a16908fdddd0d8afb
John Kyle Salaysay <foursalaysay@gmail.com>
2024-09-14 00:35:05 +0800

    change donation list cards

--- src/components/reusables/DonationListCard.tsx
+++ src/components/reusables/DonationListCard.tsx
@@ -8,6 +8,8 @@ import { usePathname } from 'next/navigation';
 import { Donation } from '@/lib/types/types';
 import ConfirmParticipation from './ConfirmParticipation';
 
+
+
 export default function DonationListCard({ donations }: { donations: Donation[] }) {
   const pathname = usePathname();
 
@@ -20,15 +22,34 @@ export default function DonationListCard({ donations }: { donations: Donation[] 
             console.log('Donation object:', donation); // Debugging line
 
             return (
-              <div key={donation.donationId} className='flex flex-col w-72 lg:w-[460px] border-2 border-black p-5 rounded-md'>
+              <div key={donation.donationId} className='flex flex-col w-[320px] lg:w-[350px] border-2 border-black p-4 rounded-md'>
                 <Image
-                  className='w-10 h-14 self-center m-5'
-                  src={BloodImage}
+                  className='self-center m-2 rounded-md'
+                  src='/hospital.jpg'
+                  width={300}
+                  height={24}
                   alt='blood'
                 />
-                <h4>ID: {typeof donation.donationId === 'string' ? `${donation.donationId.substring(0, 12)}...` : 'N/A'}</h4>
-                <p className='text-sm'>Donation Date: {new Date(donation.donationDate).toDateString()}</p>
-                <p className='text-sm'>Location: {donation.address}</p>
+                <div className='flex items-center justify-start gap-1'>
+                  <Image
+                  src='/date.png'
+                  alt='calendar'
+                  width={24}
+                  height={24}
+                  />
+                  <p className='text-sm'>{new Date(donation.donationDate).toDateString()}</p>
+                </div>
+                <div className='flex items-center justify-start gap-1'>
+                  <Image
+                  src='/place.png'
+                  alt='calendar'
+                  width={24}
+                  height={24}
+                  />
+                  <p className='text-sm'>{donation.address}</p>
+                </div>
+               
+               
                 <Separator className='mt-2'/>
                 <div className='flex flex-col lg:flex-row justify-between gap-2 my-5'>
                   <p className='text-xs'>Total Participants: {donation.totalParticipants}</p>
